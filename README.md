a91ee86b9f9e3cf4ec67510b30680491138d94d9
Guenther <gzoppelt1@gmail.com>
2016-09-02 10:08:51 +0100

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+#aqs6
+##Angular 2 Quickstart, rc6
+### abandonend for it is by far inferior to ac5
+* still translates ts into js
+* uses SystenJS instead of Wenpack 
---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<div>
+        <h1>My First Angulkar 2 App</h1>
+    </div>`
+})
+export class AppComponent {}
